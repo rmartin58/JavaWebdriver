99c5b9ff64d10525c9bdeced254ad37b430ff537
Rick Martin <rick@bearblu.com>
2021-01-03 10:27:22 -0800

    Made locators final.

--- JavaWebdriver/src/main/java/pages/DynamicLoadingPage.java
+++ JavaWebdriver/src/main/java/pages/DynamicLoadingPage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.WebDriver;
 
 public class DynamicLoadingPage {
 
-    private WebDriver driver;
-    private String linkXpath_Format = ".//a[contains(text(), '%s')]";
-    private By link_Example1 = By.xpath(String.format(linkXpath_Format, "Example 1"));
-    private By link_Example2 = By.xpath(String.format(linkXpath_Format, "Example 2"));
+    private final WebDriver driver;
+    private final String linkXpath_Format = ".//a[contains(text(), '%s')]";
+    private final By link_Example1 = By.xpath(String.format(linkXpath_Format, "Example 1"));
+    private final By link_Example2 = By.xpath(String.format(linkXpath_Format, "Example 2"));
 
-    public DynamicLoadingPage(WebDriver driver){
+    public DynamicLoadingPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public DynamicLoadingExample1Page clickExample1(){
+    public DynamicLoadingExample1Page clickExample1() {
         driver.findElement(link_Example1).click();
         return new DynamicLoadingExample1Page(driver);
     }
