64800a14c36c13c5fbb7fa1a3f501f6e450b1678
Rick Martin <rick@bearblu.com>
2021-01-03 10:20:59 -0800

    Make locators final.

--- JavaWebdriver/src/main/java/pages/AlertsPage.java
+++ JavaWebdriver/src/main/java/pages/AlertsPage.java
@@ -5,20 +5,20 @@ import org.openqa.selenium.WebDriver;
 
 public class AlertsPage {
 
-	private WebDriver driver;
-	private By triggerAlertButton = By.xpath(".//button[contains(text(),'Click for JS Alert')]");
-	private By triggerConfirmButton = By.xpath(".//button[contains(text(),'Click for JS Confirm')]");
-	private By triggerPromptButton = By.xpath(".//button[contains(text(),'Click for JS Prompt')]");
-	private By results = By.id("result");
-	
+	private final WebDriver driver;
+	private final By triggerAlertButton = By.xpath(".//button[contains(text(),'Click for JS Alert')]");
+	private final By triggerConfirmButton = By.xpath(".//button[contains(text(),'Click for JS Confirm')]");
+	private final By triggerPromptButton = By.xpath(".//button[contains(text(),'Click for JS Prompt')]");
+	private final By results = By.id("result");
+
 	public AlertsPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
 	public void triggerAlert() {
 		driver.findElement(triggerAlertButton).click();
 	}
-	
+
 	public void alert_clickToAccept() {
 		driver.switchTo().alert().accept();
 	}
