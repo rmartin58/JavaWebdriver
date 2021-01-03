7154025f3e8cc8c71f4125206851a87d8f4cf2ce
Rick Martin <rick@bearblu.com>
2021-01-03 10:31:05 -0800

    Made locator a private final field.

--- JavaWebdriver/src/main/java/pages/EmailSentConfirmationPage.java
+++ JavaWebdriver/src/main/java/pages/EmailSentConfirmationPage.java
@@ -4,13 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class EmailSentConfirmationPage {
-    private WebDriver driver;
+    private final WebDriver driver;
+    private final By textId = By.id("content");
 
     public EmailSentConfirmationPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public String getContentText() {
-        return driver.findElement(By.id("content")).getText();
+        return driver.findElement(textId).getText();
     }
 }
