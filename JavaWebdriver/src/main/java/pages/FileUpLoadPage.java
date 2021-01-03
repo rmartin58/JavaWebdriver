f1a86ac56ff575506533cde8080254e975bdf1f7
Rick Martin <rick@bearblu.com>
2021-01-03 10:37:34 -0800

    Made fields private final and moved initialization into constructor.

--- JavaWebdriver/src/main/java/pages/FileUpLoadPage.java
+++ JavaWebdriver/src/main/java/pages/FileUpLoadPage.java
@@ -5,14 +5,21 @@ import org.openqa.selenium.WebDriver;
 
 public class FileUpLoadPage {
     private final WebDriver driver;
-    private By inputField = By.id("file-upload");
-    private By uploadButton = By.id("file-submit");
-    private By uploadedFiles = By.id("uploaded-files");
+    private final By inputField;
+    private final By uploadButton;
+    private final By uploadedFiles;
 
 
-    public FileUpLoadPage(WebDriver driver) { this.driver = driver; }
+    public FileUpLoadPage(WebDriver driver) {
+        this.driver = driver;
+        inputField = By.id("file-upload");
+        uploadButton = By.id("file-submit");
+        uploadedFiles = By.id("uploaded-files");
+    }
 
-    public void clickUploadButton() { driver.findElement(uploadButton).click(); }
+    public void clickUploadButton() {
+        driver.findElement(uploadButton).click();
+    }
 
     public void uploadFile(String absolutePathOfFile) {
         driver.findElement(inputField).sendKeys(absolutePathOfFile);
