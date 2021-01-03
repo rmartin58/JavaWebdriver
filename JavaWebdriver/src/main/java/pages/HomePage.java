66c191342c12a187b543d49ca2aeb973de6399a9
Rick Martin <rick@bearblu.com>
2021-01-03 11:08:30 -0800

    Made fields private final and moved initialization into constructor; renamed link fields.

--- JavaWebdriver/src/main/java/pages/HomePage.java
+++ JavaWebdriver/src/main/java/pages/HomePage.java
@@ -5,17 +5,32 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
 
-    private WebDriver driver;
-    private final String formAuthentication = "Form Authentication";
-    private final String dropDown = "Dropdown";
-    private final String forgotPassword = "Forgot Password";
-    private final String hovers = "Hovers";
-    private final String checkboxes = "Checkboxes";
-    private final String fileUpload = "File Upload";
-    private final String fileDownLoad = "File Download";
+    private final WebDriver driver;
+    private final String formAuthenticationLink;
+    private final String dropDownLink;
+    private final String forgotPasswordLink;
+    private final String hoversLink;
+    private final String checkboxesLink;
+    private final String fileUploadLink;
+    private final String fileDownLoadLink;
+    private final String contextMenuLink;
+    private final String jsAlertsLink;
+    private final String dynamicLoadingLink;
+    private final String multipleWindowsLink;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        formAuthenticationLink = "Form Authentication";
+        dropDownLink = "Dropdown";
+        forgotPasswordLink = "Forgot Password";
+        hoversLink = "Hovers";
+        checkboxesLink = "Checkboxes";
+        fileUploadLink = "File Upload";
+        fileDownLoadLink = "File Download";
+        contextMenuLink = "Context Menu";
+        jsAlertsLink = "JavaScript Alerts";
+        dynamicLoadingLink = "Dynamic Loading";
+        multipleWindowsLink = "Multiple Windows";
     }
 
     private void clickLink(String linkText) {
@@ -23,57 +38,57 @@ public class HomePage {
     }
 
     public LoginPage clickFormAuthentication() {
-        clickLink(formAuthentication);
+        clickLink(formAuthenticationLink);
         return new LoginPage(driver);
     }
 
     public DropdownPage clickDropDown() {
-        clickLink(dropDown);
+        clickLink(dropDownLink);
         return new DropdownPage(driver);
     }
 
     public ForgotPasswordPage clickForgotPassword() {
-        clickLink(forgotPassword);
+        clickLink(forgotPasswordLink);
         return new ForgotPasswordPage(driver);
     }
 
     public HoversPage clickHovers() {
-        clickLink(hovers);
+        clickLink(hoversLink);
         return new HoversPage(driver);
     }
 
     public CheckBoxPage clickCheckBox() {
-        clickLink(checkboxes);
+        clickLink(checkboxesLink);
         return new CheckBoxPage(driver);
     }
 
     public FileUpLoadPage clickFileUpload() {
-        clickLink(fileUpload);
+        clickLink(fileUploadLink);
         return new FileUpLoadPage(driver);
     }
 
     public FileDownLoadPage clickFileDownLoad() {
-        clickLink(fileDownLoad);
+        clickLink(fileDownLoadLink);
         return new FileDownLoadPage(driver);
     }
 
     public ContextMenuPage clickContextMenu() {
-        clickLink("Context Menu");
+        clickLink(contextMenuLink);
         return new ContextMenuPage(driver);
     }
 
     public AlertsPage clickJavaScriptAlerts() {
-        clickLink("JavaScript Alerts");
+        clickLink(jsAlertsLink);
         return new AlertsPage(driver);
     }
 
-    public DynamicLoadingPage clickDynamicLoading(){
-        clickLink("Dynamic Loading");
+    public DynamicLoadingPage clickDynamicLoading() {
+        clickLink(dynamicLoadingLink);
         return new DynamicLoadingPage(driver);
     }
 
     public MultipleWindowsPage clickMultipleWindows() {
-        clickLink("Multiple Windows");
+        clickLink(multipleWindowsLink);
         return new MultipleWindowsPage(driver);
     }
 }
