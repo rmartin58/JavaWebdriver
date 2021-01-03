f62f1f7bf2d8b36e8c5bcc08f23e3d08bbd14e1a
Rick Martin <rick@bearblu.com>
2021-01-03 10:26:58 -0800

    Made driver locator final.

--- JavaWebdriver/src/main/java/pages/DropdownPage.java
+++ JavaWebdriver/src/main/java/pages/DropdownPage.java
@@ -4,13 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DropdownPage {
 
-    private WebDriver driver;
-    private By dropdown = By.id("dropdown");
+    private final WebDriver driver;
+    private final By dropdown = By.id("dropdown");
 
     public DropdownPage(WebDriver driver) {
         this.driver = driver;
