bf65e31d6502790f6120df4eeef59efe3826a2f1
rick-martin-unifyconsulting <68446606+rick-martin-unifyconsulting@users.noreply.github.com>
2021-01-03 16:31:03 -0800

    Used System.getPropery("User.dir") and System.getProperty("file.separator") to build file path.

--- JavaWebdriver/src/test/java/fileio/FileIOTests.java
+++ JavaWebdriver/src/test/java/fileio/FileIOTests.java
@@ -5,26 +5,31 @@ import org.testng.annotations.Test;
 import pages.FileDownLoadPage;
 import pages.FileUpLoadPage;
 
+import java.util.Map;
+import java.util.SplittableRandom;
+
 import static org.testng.Assert.assertEquals;
 
 public class FileIOTests extends BaseTests {
     private final String fileName;
     private final String filePath;
+    private final String fullPath;
+    private final String fileSeparator = System.getProperty("file.separator");
 
     public FileIOTests() {
-        filePath = "/Users/rickmartin/src/Java/JavaWebdriver/JavaWebdriver/resources/";
+        filePath = System.getProperty("user.dir") + fileSeparator + "resources";
         fileName = "sample.json";
+        fullPath = filePath + fileSeparator + fileName;
     }
 
     @Test
     public void shouldBeAbleToDoFileIO() {
-        logger.info(System.getProperty("user.dir"));
-
-        logger.debug(FileIOTests.class.getName() + ".testFileUpload");
+        logger.debug(FileIOTests.class.getName() + ".testFileUpload:");
+        logger.info("File to upload: " + fullPath);
 
         FileUpLoadPage fileUpLoadPage = homePage.clickFileUpload();
 
-        fileUpLoadPage.uploadFile(filePath + fileName);
+        fileUpLoadPage.uploadFile(fullPath);
 
         assertEquals(fileUpLoadPage.getUploadedFiles(), fileName, "Uploaded files incorrect");
 
