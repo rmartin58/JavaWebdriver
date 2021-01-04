0122c887924204491048ad51b3b733107bfc5f88
rick-martin-unifyconsulting <68446606+rick-martin-unifyconsulting@users.noreply.github.com>
2021-01-03 16:57:48 -0800

    Created a parameterized version of failed login tests.

--- JavaWebdriver/src/test/java/login/LoginTests.java
+++ JavaWebdriver/src/test/java/login/LoginTests.java
@@ -1,6 +1,7 @@
 package login;
 
 import base.BaseTests;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.EmailSentConfirmationPage;
 import pages.ForgotPasswordPage;
@@ -11,14 +12,20 @@ import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends BaseTests {
 
+    @DataProvider(name = "validCredentials")
+    private static Object[][] getValidCredentials() {
+        return new Object[][]{
+                {"tomsmith", "SuperSecretPassword!", "You logged into a secure area!"},
+        };
+    }
 
-    @Test
-    public void shouldBeAbleToSuccessfullyLogin() {
+    @Test(dataProvider = "validCredentials")
+    public void shouldBeAbleToSuccessfullyLogin(String userName, String passWord, String msg) {
         LoginPage loginPage = homePage.clickFormAuthentication();
-        loginPage.setUsernameField("tomsmith");
-        loginPage.setPasswordField("SuperSecretPassword!");
-        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
-        assertTrue(secureAreaPage.getAlertText().contains("You logged into a secure area!"), "Alert text is incorrect");
+        loginPage.setUsernameField(userName);
+        loginPage.setPasswordField(passWord);
+        var secureAreaPage = loginPage.clickLoginButton();
+        assertTrue(secureAreaPage.getAlertText().contains(msg), "Alert text is incorrect");
     }
 
     @Test
@@ -46,4 +53,22 @@ public class LoginTests extends BaseTests {
         EmailSentConfirmationPage emailSentConfirmationPage = forgotPasswordPage.clickRetrievePasswordButton();
         assertTrue(emailSentConfirmationPage.getContentText().contains("Your e-mail's been sent"));
     }
+
+    @DataProvider(name = "inValidCredentials")
+    private static Object[][] getInvalidCredentials() {
+        return new Object[][]{
+                {"tomsmith", "BadPassword", "Your password is invalid!"},
+                {"BadUserName", "SuperSecretPassword!", "Your username is invalid!"}
+        };
+    }
+
+    @Test(dataProvider = "inValidCredentials")
+    public void shouldDisplayUnsuccessfulLoginMsg(String userName, String passWord, String msg) {
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUsernameField(userName);
+        loginPage.setPasswordField(passWord);
+        loginPage.clickLoginButton();
+        assertTrue(loginPage.getAlertText().contains(msg), "Alert text is incorrect");
+    }
+
 }
